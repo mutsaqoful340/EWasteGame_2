cb4e658d791ab0eb839d604504bf55379a0e483f
Fikri Ahmad <rukimevick@gmail.com>
2025-04-21 22:44:09 +0700

    Pause Menu Yes
    Iyah

--- Assets/Script/UI Scripts/UIAnim_Script.cs
+++ Assets/Script/UI Scripts/UIAnim_Script.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
     public class Anim_Manager : MonoBehaviour
     {
-        public GameObject popUp;
-        private Animator animator1;
-        //animator1> popup;
+        public GameObject popPCB, popMenuButtons;
+        private Animator animator1, animator2;
+        //animator1 > popup PCB
+        //animator2 > popup MenuButtons
 
         private void Start()
         {
-            animator1 = popUp.GetComponent<Animator>();
+            animator1 = popPCB.GetComponent<Animator>();
+            animator2 = popMenuButtons.GetComponent<Animator>();
         }
 
         public void ShowPopup()
         {
             animator1.Play("PCB_IN");
+        animator2.Play("BTN_IN");
         }
 
         public void HidePopup()
         {
             animator1.Play("PCB_OUT");
+        animator2.Play("BTN_OUT");
         }
     }
