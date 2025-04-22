fa7e7fba7d6157424d16401d66a26c86a2f8baaa
Fikri Ahmad <rukimevick@gmail.com>
2025-04-22 20:14:58 +0700

    Adding Audio & PauseMenu Butto
    Adding BGM and Click SFX. As well pause function which Esc button is used to call the PauseMenu popup.

--- Assets/Script/UI Scripts/UIAnim_Script.cs
+++ Assets/Script/UI Scripts/UIAnim_Script.cs
@@ -4,26 +4,56 @@ using UnityEngine;
 
     public class Anim_Manager : MonoBehaviour
     {
+        Audio_Manager audioManager;
+
+        private void Awake()
+        {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<Audio_Manager>();
+
+        }
+
         public GameObject popPCB, popMenuButtons;
         private Animator animator1, animator2;
         //animator1 > popup PCB
         //animator2 > popup MenuButtons
+
+        private bool isPaused = false; // track menu state
 
         private void Start()
         {
             animator1 = popPCB.GetComponent<Animator>();
             animator2 = popMenuButtons.GetComponent<Animator>();
         }
-
-        public void ShowPopup()
+        
+        
+        private void Update()
         {
-            animator1.Play("PCB_IN");
-        animator2.Play("BTN_IN");
+            if (Input.GetKeyDown(KeyCode.Escape)) // To open Pause Menu popup
+            {
+                if (isPaused)
+                {
+                   HidePopup();
+                }
+                else
+                {
+                    ShowPopup();
+                }
+
+                isPaused = !isPaused;
+            }
         }
 
-        public void HidePopup()
+        public void ShowPopup() // To show popup
         {
+            audioManager.PlaySFX(audioManager.buttonClick);
+            animator1.Play("PCB_IN");
+            animator2.Play("BTN_IN");
+        }
+
+        public void HidePopup() // To hide popup
+        {
+            audioManager.PlaySFX(audioManager.buttonClick);
             animator1.Play("PCB_OUT");
-        animator2.Play("BTN_OUT");
+            animator2.Play("BTN_OUT");
         }
     }
